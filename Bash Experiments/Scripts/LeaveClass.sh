f10ee5e32677f295ebe7a586a43ae78e2f21c259
brainchemist2005 <zackariaebouargan@gmail.com>
2025-01-20 18:44:18 -0500

    This script helps you exit an online meeting at a specific time

---
+++ Bash Experiments/Scripts/LeaveClass.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+value=false
+
+echo "Hello what time does your meeting end ? (HH:MM:AM/PM) format"
+echo "Here is an example $(date +"%I:%M %p")"
+read time
+
+echo "Are you using zoom or teams ?"
+read client 
+
+
+
+while [ "$value" = false ]; do
+    if [[ "$(date +"%I:%M %p")" == $time ]]; then
+        value=true
+        kill -9 $(pgrep $client)
+    fi
+
+    sleep 60
+done
